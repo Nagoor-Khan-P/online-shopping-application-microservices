89296bc7d62547d24db7b11b5cd3e3df3e8c6d06
Nagoor-Khan-P <106923967+Nagoor-Khan-P@users.noreply.github.com>
2024-08-14 23:21:51 +0530

    Resolved services routing issue

---
+++ api-gateway/src/main/java/com/nagoorkhan/apigateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.nagoorkhan.apigateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity security) {
+        security.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return security.build();
+    }
+}
